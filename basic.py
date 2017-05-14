527bbb623f590e1a8986c991258c6e2b3108d576
Chao-Ming Chang <cmc4010@gmail.com>
2017-05-14 17:42:52 +0800

    added custom binning

--- basic.py
+++ basic.py
@@ -23,6 +23,8 @@ dataset = pandas.read_csv(uri, sep=',', index_col = 0, names = names)
 
 print(dataset.shape)
 
+print(dataset)
+
 description = dataset.describe()
 
 # print(description)
@@ -50,6 +52,54 @@ featureRange = ['SS-IN', 'SED-IN', 'COND-IN', 'SS-OUT', 'SED-OUT', 'COND-OUT']
 
 # continuous features
 # SOLUTIONS: normal distribution, exponential distribution
+
+# BINNING + SMOOTHING
+
+# SS-IN: 3 bins... or 2
+# -> boundary (2 bins): (256 + 964)/2
+# SED-IN: 4 bins
+# -> boundary (2 bins): (7 + 17)/2
+# COND-IN: 2 big bins
+# -> boundary (2 bins): (1410 + 1814)/2
+# SS-OUT: 4 bins
+# -> boundary (3 bins): (27+73)/2, (82+104)/2
+# SED-OUT: 3 bins
+# -> boundary (2 bins): (3.5+1030)/2
+# COND-OUT: 3 bins
+# -> boundary (3 bins): (43.1+1221)/2, (1575+1879)/2
+
+# discretize into bin number 0, 1, (2)
+# custom binning code
+
+boundaries = [
+	[(256+964)/2], [(7 + 17)/2], [(1410 + 1814)/2],
+	[(27+73)/2, (82+104)/2], [(3.5+1030)/2], [(43.1+1221)/2, (1575+1879)/2]
+]
+
+# given dataset, boundaries, and feature range
+# we can bin the dataset
+def binning( dataset, boundaryList, featureList ):
+	binnedDataset = dataset.values
+	for idx, instance in enumerate(binnedDataset):
+		# print instance
+		for feature in range(0, len(featureList)):
+			# 0~5 feature
+			# perform for each feature
+			updatedFlag = 0
+			for bid, boundary in enumerate(boundaryList[feature]):
+				# print boundary
+				# print bid, instance[feature]
+				if instance[feature] < boundary:
+					instance[feature] = bid
+					updatedFlag = 1
+					break;
+			# handle greater than boundary
+			if updatedFlag == 0:
+				instance[feature] = len(boundaryList[feature])
+	# return 0
+	return pandas.DataFrame(binnedDataset, index=dataset.index.values, columns=dataset.columns.values)
+
+binnedData = binning( dataset, boundaries, featureRange )
 
 # --- Create a naive Bayes model ---
 
@@ -91,20 +141,47 @@ def exponentialDist( value, lamb ):
 
 # print exponentialDist( 222, "SS-IN")
 
-def basicNaiveBayesModel( query, featureList, targetLevels, method ):
+def binQuery( query, boundaryList, featureList ):
+	# value: query value
+	# idx: index of which feature
+	# boundaryList: list of boundaries to discretize the value
+	binnedValues = []
+	for feature in range(0, len(featureList)):
+		updatedFlag = 0
+		binnedValue = -1
+		for bid, boundary in enumerate(boundaryList[feature]):
+			# print boundary
+			# print bid, instance[feature]
+			if query[feature] < boundary:
+				binnedValue = bid
+				updatedFlag = 1
+				break;
+		# handle greater than boundary
+		if updatedFlag == 0:
+			binnedValue = len(boundaryList[feature])
+		binnedValues.append(binnedValue)
+	return binnedValues
+
+def basicNaiveBayesModel( data, query, featureList, targetLevels, method, datatype = "discrete", boundaryList = None):
+	# data: full dataset
 	# query: query values in list format [q1, q2, q3, q4, q5, q6]
 	# featureList: list of all descriptive features
 	# targetLevels: list of possible target levels
 	# method: type of distribution to use
 
-	# multiply the probabilties together
+	# multiply the probabilities together
 	# IMPORTANT: the mean and std is calculated from subset of data
 	# that has target that specific target value
 	results = []
 	for target in targetLevels:
 		# get subset based on target level
-		targetSubset = dataset[dataset['STATUS']==target]
-		targetProb = len(targetSubset)/float(len(dataset))
+		targetSubset = data[data['STATUS']==target]
+		targetProb = len(targetSubset)/float(len(data))
+
+		# BIN QUERY FOR CONTINUOUS DATA
+		if method == "discrete" and datatype == "continuous":
+			query = binQuery( query, boundaryList, featureList )
+			# print binnedQuery
 
 		# PERFORM CALCULATIONS
 		# query -- 6 entries
@@ -112,15 +189,32 @@ def basicNaiveBayesModel( query, featureList, targetLevels, method ):
 		total = 1
 		for idx in range(0,len(featureList)):
 			feature = featureList[idx]
+			# print idx
 			# NORMAL
 			if method == "normal":
 				u = targetSubset.mean(axis=0)[feature]
 				o = targetSubset.std(axis=0)[feature]
 				total *= normalDist( query[idx], u, o )
+				# print normalDist( query[idx], u, o )
 			# EXPONENTIAL
 			elif method == "exp":
-				lamb = 1 / dataset.mean(axis=0)[feature]
+				lamb = 1 / targetSubset.mean(axis=0)[feature]
 				total *= exponentialDist( query[idx], lamb)
+				# print exponentialDist( query[idx], lamb)
+			elif method == "discrete":
+				# perform the most basic naive Bayes model operations
+				# for each feature
+				# 	check the relative frequency within target subset
+				# PROBLEM: I have to handle "Empty dataframe"
+				# SOLUTION: data smoothing
+
+				# subset of values equal to the query value
+				eqVal = targetSubset[targetSubset[feature] == query[idx]]
+				# count of the values equal to the query value
+				freq = len(eqVal)
+				# the probability of this query value within the subset
+				relFreq = float(freq)/len(targetSubset)
+				print relFreq
 			else:
 				print("Error in choosing distribution")
 		results.append(total*targetProb)
@@ -132,4 +226,6 @@ def basicNaiveBayesModel( query, featureList, targetLevels, method ):
 	return targetLevels[indexMax]
 
 myQuery = [222, 4.5, 1518, 74, 0.25, 1642]
-print basicNaiveBayesModel( myQuery, featureRange, targetValueRange, "exp" )
+# print basicNaiveBayesModel( dataset, myQuery, featureRange, targetValueRange, "normal" )
+# print basicNaiveBayesModel( dataset, myQuery, featureRange, targetValueRange, "exp" )
+basicNaiveBayesModel( binnedData, myQuery, featureRange, targetValueRange, "discrete", "continuous", boundaries )
